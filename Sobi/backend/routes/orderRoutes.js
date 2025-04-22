918980d5ce87a35d6d27e5583df3ee0984eb7c97
Sobiga11 <123938231+Sobiga11@users.noreply.github.com>
2025-04-22 14:28:57 +0530

    Delete Sobi/backend directory

--- Sobi/backend/routes/orderRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const orderController = require('../controllers/orderController');
-const auth = require('../middleware/auth');
-const router = express.Router();
-
-router.get('/', auth, orderController.getOrders);
-router.post('/create', auth, orderController.createOrder);
-module.exports = router;
